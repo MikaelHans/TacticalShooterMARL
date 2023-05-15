6cba9be98b8764f1d7b50b741b1c2c8b5dea0b8d
MikaelHans <73513335+MikaelHans@users.noreply.github.com>
2023-05-16 02:02:38 +0700

    Update AudioEmiterEditor.cs

--- Assets/Editor/AudioEmiterEditor.cs
+++ Assets/Editor/AudioEmiterEditor.cs
@@ -1,9 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEditor;
-using UnityEditor.Experimental.TerrainAPI;
-using log4net.Util;
+
 
 [CustomEditor(typeof(AudioEmiter))]
 
